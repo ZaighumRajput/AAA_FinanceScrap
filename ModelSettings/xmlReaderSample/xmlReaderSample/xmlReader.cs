60e1484efc8717588d32250aba995c1812fb00f8
zaighumrajput <zaighum47@gmail.com>
2014-10-15 18:15:17 +0200

    C# XMLreader, FX rates from ECB

---
+++ ModelSettings/xmlReaderSample/xmlReaderSample/xmlReader.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml;
+using System.IO;
+using System;
+
+namespace xmlReaderSample
+{
+    class reader
+    {
+        public static void Read(string fileName)
+        {
+            string path = Directory.GetCurrentDirectory()  ;
+            string fullPathName = System.IO.Path.Combine(path, fileName);
+            XmlReader xmlReader = XmlReader.Create(fullPathName);
+            while (xmlReader.Read())
+            {
+                 if((xmlReader.NodeType == XmlNodeType.Element) && (xmlReader.Name == "Cube"))
+                {
+                    if(xmlReader.HasAttributes)
+                        Console.WriteLine(xmlReader.GetAttribute("currency") + ": " + xmlReader.GetAttribute("rate"));                    
+                }
+            }
+            Console.ReadKey();
+
+            }
+        }
+    }
+
+
