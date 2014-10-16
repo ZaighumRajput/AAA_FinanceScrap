b18b6acabdcbda4635be7d3006d7e680c9293bea
zaighumrajput <zaighum47@gmail.com>
2014-10-16 13:22:55 +0200

    C# xmlDocument example

---
+++ ModelSettings/xmlReaderSample/xmlReaderSample/xmlDocument.cs
@@ -0,0 +1,28 @@
+﻿using System.Xml;
+using System;
+
+namespace xmlReaderSample
+{
+    class xmlDocument
+    {
+        public static void Read(string path, string fileName)
+        {
+            string fullPathName = System.IO.Path.Combine(path, fileName);
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(fullPathName);
+
+            foreach (XmlNode xmlNode in xmlDoc.DocumentElement.ChildNodes[2].ChildNodes[0].ChildNodes)
+                Console.WriteLine(xmlNode.Attributes["currency"].Value + ": " + xmlNode.Attributes["rate"].Value);
+            Console.ReadKey();
+
+        }
+            
+
+
+        }
+
+
+    }
+
+
